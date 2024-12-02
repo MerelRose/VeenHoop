ed9358e9c30a41d4ef40fd952c8334a2968823dd
Merel Rose <mrosedevries@gmail.com>
2024-12-02 12:55:57 +0100

    tailwind

--- Frontend/veenhoop/src/App.js
+++ Frontend/veenhoop/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
-import logo from './images/logo.png';
+import logo from './images/logo_blop.png';
 import './styles/App.css';
 import Login from './pages/Login'; // Ensure Home is properly exported from Home.js
 
@@ -9,8 +9,9 @@ function Header() {
   const navigate = useNavigate();
 
   return (
-    <header>
-      <img src={logo} className="logo" alt="logo" onClick={() => navigate('/')} />
+    <header className='w-screen bg-cyan-700'>
+      <img src={logo} className="w-48 h-auto" alt="logo" onClick={() => navigate('/')} />
+      <h1>Login</h1>
     </header>
   );
 }
---
+++ Frontend/veenhoop/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this covers all React component files
+  ],
+  theme: {
+    extend: {
+      colors: {
+      lichtblauw: 'rgb(14, 146, 202)',  // Light Blue
+      donkerblauw: 'rgb(0, 105, 141)',  // Dark Blue
+    },},
+  },
+  plugins: [],
+};
