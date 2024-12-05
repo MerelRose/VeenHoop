704f60ee00a9aad045843303f5fe6a2c4545b554
Merel Rose <123648592+MerelRose@users.noreply.github.com>
2024-12-05 09:15:14 +0100

    css

--- Frontend/veenhoop/src/pages/Login.js
+++ Frontend/veenhoop/src/pages/Login.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <>
     <img src={background} className="absolute w-[70%] h-auto left-[15%]" alt="bg"/>
-    <div className='absolute left-[40%] top-[30%] bg-white'>
+    <div className='absolute left-[40%] top-[30%] bg-white p-5 h-[45%] rounded-lg'>
       <form>
         <label for='email' className='text-lg font-Rubik'>School e-mail:</label><br/>
         <input type="text" id="email" name="email" placeholder='School e-mail' className='p-2 rounded-lg w-96 border-inherit bg-gradient-to-r from-stone-300 to-stone-200'></input>
@@ -20,7 +20,7 @@ function Home() {
         <button type="button" className="w-[185px] text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Student</button>
         <button type="button" className="w-[185px] text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Leraar</button>
       <br/>
-        <button type="button" class="w-[185px] py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Login</button>
+        <button type="button" class="bottom-5 absolute w-[185px] py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Login</button>
       </div>
     </>
   );
